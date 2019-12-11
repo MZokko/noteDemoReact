ab7c40dc3592cc4e9894e308f47c75582808fc3e
alexandre <akekiwy@gmail.com>
2019-12-11 14:21:50 +1100

    1st comit add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyC6cY4kYX9mk5fbDIdq9PKELWgdlpWm7uw",
+    authDomain: "notes-de.firebaseapp.com",
+    databaseURL: "https://notes-de.firebaseio.com",
+    projectId: "notes-de",
+    storageBucket: "notes-de.appspot.com",
+    messagingSenderId: "757493063062",
+    appId: "1:757493063062:web:40d29fe9404d4133ed91b7",
+    measurementId: "G-D57LF0WTMV"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyC6cY4kYX9mk5fbDIdq9PKELWgdlpWm7uw",
+    authDomain: "notes-de.firebaseapp.com",
+    databaseURL: "https://notes-de.firebaseio.com",
+    projectId: "notes-de",
+    storageBucket: "notes-de.appspot.com",
+    messagingSenderId: "757493063062",
+    appId: "1:757493063062:web:40d29fe9404d4133ed91b7",
+    measurementId: "G-D57LF0WTMV"
+  }
 };
 
 /*
